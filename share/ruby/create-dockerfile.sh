61684c685846cd651a0c3919eb88ad7b25a8ffd8
Rahul Sinha <rahul@rahulwa.com>
2018-09-23 11:20:13 +0530

    create dockerfile is conceptually complete

--- share/ruby/create-dockerfile.sh
+++ share/ruby/create-dockerfile.sh
@@ -34,16 +34,31 @@ append_dependencies_in_dockerfile() {
   # Only retain uniq values
   dependencies_list=($(printf "%s\n" "${all_dependencies_list[@]}" | sort -u))
   for pkg in "${dependencies_list][@]}"; do
-    if [[ $pkg != ${dependencies_list[-1]} ]]; then
-      awk '1;/\$DEFAULT_PACKAGES/{print "'"$pkg"'  && \\";next}' $_ji_CURRENT_APP_PATH/Dockerfile
-    elif
-      awk '1;/\$DEFAULT_PACKAGES/{print "'"$pkg"'";next}' $_ji_CURRENT_APP_PATH/Dockerfile
+      # if [[ $pkg != ${dependencies_list[-1]} ]]; then
+    awk '1;/\$DEFAULT_PACKAGES/{print "'"$pkg"' \\";next}' $_ji_CURRENT_APP_PATH/Dockerfile
+      # else
+      #   awk '1;/\$DEFAULT_PACKAGES/{print "'"$pkg"' && \\";next}' $_ji_CURRENT_APP_PATH/Dockerfile
+      # fi
+  done
+  awk '/# Other dependencies/{print "   && rm -rf /var/cache/apk/*"}1' $_ji_CURRENT_APP_PATH/Dockerfile
+}
+
+add_extra_dependencies_in_dockerfile() {
+  for gem in "${installed_pkg_list[@]}"; do
+    dependencies=`awk '/dependencies = /,/END/ { gsub("dependencies = ",""); gsub("END",""); print }' $_JI_LANGUAGE_PATH/gems/$gem | awk 'NF'`
+    if [[ $dependencies -ge 5 ]]; then
+      awk '1;/# Other dependencies/{print "Needed by '"$gem"'";next}' $_ji_CURRENT_APP_PATH/Dockerfile
+      awk '1;/# Other dependencies/{print "RUN '"$dependencies"'";next}' $_ji_CURRENT_APP_PATH/Dockerfile
     fi
   done
 }
 
-add_extra_dependencies_in_dockerfile() {
-  all_dependencies_list=()
+add_build_parameters_in_dockerfile() {
   for gem in "${installed_pkg_list[@]}"; do
-    awk '/dependencies = /,/END/ { gsub("dependencies = ",""); gsub("END",""); print }' $_JI_LANGUAGE_PATH/gems/$gem | awk 'NF'
+    buildConfig=`awk '/buildConfig = /,/END/ { gsub("buildConfig = ",""); gsub("END",""); print }' $_JI_LANGUAGE_PATH/gems/$gem | awk 'NF'`
+    if [[ $buildConfig -ge 5 ]]; then
+      awk '1;/# Build Config/{print "Needed by '"$gem"'";next}' $_ji_CURRENT_APP_PATH/Dockerfile
+      awk '1;/# Build Config/{print "RUN '"$buildConfig"'";next}' $_ji_CURRENT_APP_PATH/Dockerfile
+    fi
+  done
 }
